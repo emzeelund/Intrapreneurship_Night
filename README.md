f4d8cfc4345ad7ce07cec28e553a01209b1a9909
Mark Lund <marklund14@yahoo.com>
2018-10-15 13:47:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Intrapreneurship_Night
---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<htmL>
+<head>
+  <title>Intrapreneurship Night</title>
+  <!--<meta charset="utf-8">-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="icon" href="img/favicon.png">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/customized.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet">
+
+
+  <style>
+        .wow:first-child
+        {
+            visibility: hidden;
+        }
+  </style>
+
+  <script src="js/jquery.min.js" type="text/javascript"></script>
+  <script src="js/wow.min.js" type="text/javascript"></script>
+  <script>new WOW().init();</script>
+
+</head>
+<body>
+
+<header class="mainheader">
+  <div class="container logo"><img src="img/247_logo.svg" style="width: 30%;"/></div>
+  <div class="container intra_2017 wow pulse" data-wow-iteration="infinite"><img src="img/2017_Intra.svg" style=" width: 100%;"/></div>
+</header>
+
+<section class="section1">
+  <div class="container engage_logo wow rotateIn"><img src="img/engage_logo.svg" id="engage" style="width: 20%; margin: auto; display: block;" /></div>
+  <div class="container time_date"><h2 style="text-align: center; margin-top: 1em; margin-bottom: 2em;"><strong>April 12, 2017 | 7:00 pm </strong><br/> Oakwood Joy-Nostalg Premier</h2></div>
+  <div class="container"><a href="#"><button type="button" class="btn btn-primary btn-lg reserve">Reserve Now!</button></a></div>
+</section>
+
+<section class="section2">
+
+  <div class="container desc"><h3 style="text-align: center; margin-top: 1em; margin-bottom: 1em;"><strong>2017 INITIATIVES & DESCRIPTIONS</strong></h3></div>
+  <div class="container"><hr class="desc_sep"></hr></div>
+  <div class="container invite"><p style="text-align: center; margin-top: 1em; margin-bottom: 1em; font-size:1.5em;">You are cordially invited to attend our annual Intrapreneurship Night </p></div>
+</section>
+
+<section class="section3">
+
+  <div class="container">
+    <div class="row initiative_row">
+      <div class="col-4">
+        <div class="image_container pink wow bounceInUp"><img src="img/icons/chart-bar.svg" class="wow tada" data-wow-duration="4s" data-wow-iteration="infinite" /></div>
+      </div>
+      <div class="col-8 wow fadeInRight">
+          <h2>Squadron Leader Engagement Program</h2>
+          <p>Squadron Leader Development and Engagement through mentorship and immersion. This program trains leaders on Service Delivery Management, Client Management and Sales.</p>
+      </div>
+    </div>
+
+      <div class="container"><hr class="sep_mobile"></hr></div>
+
+    <div class="row initiative_row">
+      <div class="col-8 wow fadeInLeft">
+          <h2>Recruitment to Handholding Program</h2>
+          <p>All resource partners 1 year and above may refer new partners to join 24/7. The referrer must then accommodate and inculturate the new hire for 1 year. </p>
+      </div>
+      <div class="col-4 wow bounceInUp">
+        <div class="image_container red"><img src="img/icons/handshake.svg" class="wow bounce"  data-wow-duration="4s" data-wow-iteration="infinite"/></div>
+      </div>
+    </div>
+
+    <div class="container"><hr class="sep_mobile"></hr></div>
+
+  <div class="row initiative_row">
+    <div class="col-4 wow bounceInUp">
+      <div class="image_container lightgrey"><img src="img/icons/hands.svg" class="wow pulse" data-wow-iteration="infinite"/></div>
+    </div>
+    <div class="col-8 wow fadeInRight">
+        <h2>Angel / Buddy Program (Hiring to Hand-Holding)</h2>
+        <p>To have a qualified angel/ buddy to conduct handholding for all new hires.</p>
+    </div>
+  </div>
+
+    <div class="container"><hr class="sep_mobile"></hr></div>
+
+  <div class="row initiative_row">
+    <div class="col-8 wow fadeInLeft">
+        <h2>Exodus Program</h2>
+        <p>Newly hired (less than 1 year) partners are encouraged to share something they like about 24/7 to friends and previous colleagues.  They may also refer interested candidates who will be guided by qualified resource partners during application processing.</p>
+    </div>
+    <div class="col-4 wow bounceInUp">
+      <div class="image_container darkgrey"><img src="img/icons/route.svg" class="wow pulse" data-wow-iteration="infinite"/></div>
+    </div>
+  </div>
+
+    <div class="container"><hr class="sep_mobile"></hr></div>
+
+  <div class="row initiative_row">
+    <div class="col-4 wow bounceInUp">
+      <div class="image_container red"><img src="img/icons/plus-circle.svg" class="wow tada"  data-wow-duration="4s" data-wow-iteration="infinite"/></div>
+    </div>
+    <div class="col-8 wow fadeInRight">
+        <h2>Positive Recognition Program (PRP)</h2>
+        <p>This program aims to formally recognize an outstanding performance and achievement of a resource partner.</p>
+    </div>
+  </div>
+
+  <div class="container"><hr class="sep_mobile"></hr></div>
+
+  <div class="row initiative_row">
+    <div class="col-8 wow fadeInLeft">
+        <h2>Partner Acceleration Thru Project Onboarding Program</h2>
+        <p>Project Onboarding is a process that helps in the acceleration of a resource partner into becoming an A-player as it opens channels for communication and feedback, and effective performance management.</p>
+    </div>
+    <div class="col-4 wow bounceInUp">
+      <div class="image_container pink"><img src="img/icons/angle-double-right.svg" class="wow shake" data-wow-duration="4s" data-wow-iteration="infinite"/></div>
+    </div>
+  </div>
+
+  <div class="container"><hr class="sep_mobile"></hr></div>
+
+  <div class="row initiative_row">
+    <div class="col-4 wow bounceInUp">
+      <div class="image_container darkgrey"><img src="img/icons/chalkboard-teacher.svg" class="wow pulse" data-wow-iteration="infinite"/></div>
+    </div>
+    <div class="col-8 wow fadeInRight">
+        <h2>Intrapreneurship Orientation</h2>
+        <p>Intrapreneurship Orientation (New Hire) <br/>To introduce intrapreneurship and how to become one. <br/>Intrapreneurship Refresher (for Existing Partners) <br/>Refresher activities will be embedded into 24/7 events and activities so we can continually grow as intrapreneurs.</p>
+    </div>
+  </div>
+
+  <div class="container"><hr class="sep_mobile"></hr></div>
+
+  <div class="row initiative_row">
+    <div class="col-8 wow fadeInLeft">
+        <h2>Standard Insourcing Service Delivery Framework </h2>
+        <p>Standardizing our Insourcing service with the help of best practices and frameworks such as ITIL.</p>
+    </div>
+    <div class="col-4 wow bounceInUp">
+      <div class="image_container lightgrey"><img src="img/icons/truck.svg" class="wow shake" data-wow-duration="4s" data-wow-iteration="infinite"/></div>
+    </div>
+  </div>
+
+  <div class="container"><hr class="sep_mobile"></hr></div>
+
+  <div class="row initiative_row">
+    <div class="col-4 wow bounceInUp">
+      <div class="image_container red"><img src="img/icons/brain.svg" class="wow rubberBand"  data-wow-duration="4s" data-wow-iteration="infinite"/></div>
+    </div>
+    <div class="col-8 wow fadeInRight">
+        <h2>Skills Intelligence Program</h2>
+        <p>This program intends to help our organization better assess our ability to meet market demands for IT services, by having a compilation of the skills, capabilities and industry knowledge of our resource-partners.</p>
+    </div>
+  </div>
+
+</div>
+
+</section>
+
+<footer>
+
+</footer>
+
+<section class="container-fluid credit" style="background-color:#ec1c24; text-align:center;color:#fff; padding: 1em"><div id="credit"> Copyright © 2018 Mark Lund. All rights reserved.</div></section>
+
+</body>
+</html>
